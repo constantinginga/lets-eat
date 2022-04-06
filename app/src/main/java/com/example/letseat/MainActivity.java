b6fa6e140ee3bfcce72f3e734eb9de94ee26644f
gof3ck69 <sorryy647@gmail.com>
2022-04-06 13:49:42 +0200

    starting with bottom navigation

--- app/src/main/java/com/example/letseat/MainActivity.java
+++ app/src/main/java/com/example/letseat/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.letseat;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView recipeName;
     private Button searchBtn;
     private EditText recipeIn;
+    private NavController navController;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         rv.hasFixedSize();
         rv.setLayoutManager(new LinearLayoutManager(this));
         ArrayList<Recipe> recipes = new ArrayList<>();
+        BottomNavigationView
 
 //        recipes.add(new Recipe(1, "Recipe1", "https://i1.sndcdn.com/artworks-000405558477-s8gy6t-t500x500.jpg"));
 //        recipes.add(new Recipe(1, "Recipe1", "https://i1.sndcdn.com/artworks-000405558477-s8gy6t-t500x500.jpg"));
@@ -78,5 +85,16 @@ public class MainActivity extends AppCompatActivity {
         // something wrong with recycler view
 
 
+
+        private void setBottomNavigationVisibility() {
+            navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+                final int id = destination.getId();
+                if (id == R.id.nav_pokedex || id == R.id.navigation_teams) {
+                    bottomNavigationView.setVisibility(View.VISIBLE);
+                } else {
+                    bottomNavigationView.setVisibility(View.GONE);
+                }
+            });
+        }
     }
 }
