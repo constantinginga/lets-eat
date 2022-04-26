781efe485720167af3752c180e60e17b3edb495a
gof3ck69 <sorryy647@gmail.com>
2022-04-26 18:19:32 +0200

    fixed bottom navigation

--- app/src/main/java/com/example/letseat/MainActivity.java
+++ app/src/main/java/com/example/letseat/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.letseat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +31,8 @@ import view.MainPageFragment;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
-        final NavController navController = navHostFragment.getNavController();
+        navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 
